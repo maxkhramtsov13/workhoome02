fd902d9119052ad447ba5fd21dcb7674fa43935d
MaxKhramtsov <maxkhramtsov13@gmail.com>
2023-01-25 11:33:30 +0300

    домашка02

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿System.Console.Write($" Введите первое число - ");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.Write($" Введите второе число - ");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+int min = a;
+
+
+if( a > max ) max = a;
+if( a < max ) min = a;
+if( b > max ) max = b;
+if( b < max ) min = a;
+
+System.Console.Write($"max - ");
+System.Console.WriteLine(max);
+System.Console.Write($"min - ");
+System.Console.WriteLine(min);
